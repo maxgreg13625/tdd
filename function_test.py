3e7bf628448cdc0771a3208274abc8c5d3b6b6b3
Rendy Kuo <maxgreg13625@gmail.com>
2020-07-19 20:08:33 +0800

    finish chapter 2

--- function_test.py
+++ function_test.py
@@ -1,8 +1,36 @@
 #! /home/rendy/python/env/tdd/bin/python
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class DjanjoAppTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Chrome()
+		self.browser.implicity_wait(5)
 
-assert 'Django' in browser.title
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_input_and_retrive_a_list(self):
+		# user open browser and view the home page of website
+		self.browser.get('http://localhost:8000')
+
+		# user notice there is 'To-Do' items in browser title
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+		# user accept and create first 'To-Do' item
+
+		# user input 'buy PS4' in the text area
+
+		# after user enter, webpage will re-fresh and display 'buy PS4' in the to do list
+
+		# there is another text area for user to input and user input 'buy P5S'
+
+		# after user enter, webpage will re-fresh and display two items
+
+		# webpage generate an unique url to user to show that website remember user's to-do list
+
+		# user click the link and found its to-do list
+
+if __name__ == '__main__':
+	unittest.main()
